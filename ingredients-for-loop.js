0e094817809481d9cfe5d6d7a22a600d65b1899b
rewirepdq <rewirepdq@mail.com>
2017-06-20 22:39:46 +0000

    Adds looping exercises

---
+++ ingredients-for-loop.js
@@ -0,0 +1,4 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for (var i = 0; i <= 7; i = i + 1)
+console.log(ingredients[i]);
---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var i = 0;
+
+while (i < 8) {
+  console.log(ingredients[i]);
+  i++
+}
+
+for (var i = 0; i <= 7; i = i + 1)
+console.log(ingredients[i]);
+
+var j = 7;
+for (j = 7; j >= 0; j--) {
+  console.log(ingredients[j]);
+
+}
